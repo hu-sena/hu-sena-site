764dd22c56d783b2a84b982309bbf57489d8c239
husna <shzubir@seekasia.com>
2025-11-02 17:44:20 +0800

    Quartz sync: Nov 2, 2025, 5:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Playfair Display",
-        body: "Source Sans Pro",
+        body: "Playfair Display",
         code: "IBM Plex Mono",
       },
       colors: {
