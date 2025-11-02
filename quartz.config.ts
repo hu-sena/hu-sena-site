28e91e23ff517fd7b3464c7ac92dee373ce2290c
husna <shzubir@seekasia.com>
2025-11-02 17:50:14 +0800

    Quartz sync: Nov 2, 2025, 5:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Playfair Display",
-        body: "Playfair Display",
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
       colors: {
